ae25f8ea7e4f3dc7bfeb2d756eb54bacaa90a870
dadda <dadda.digioia@gmail.com>
2020-02-22 20:15:43 +0000

    First Commit

---
+++ K-means.py
@@ -0,0 +1,48 @@
+
+from copy import deepcopy
+from sklearn.datasets import load_iris
+import numpy as np
+
+data=load_iris()
+
+X=data['data']
+y=data['target']
+K = 3
+# Number of training data
+n = X.shape[0]
+# Number of features in the data
+c = X.shape[1]
+
+# Generate random centers, here we use sigma and mean to ensure it represent the whole data
+mean = np.mean(X, axis = 0)
+std = np.std(X, axis = 0)
+centers = np.random.randn(K,c)*std + mean
+
+centers_old = np.zeros(centers.shape) # to store old centers
+centers_new = deepcopy(centers) # Store new centers
+
+clusters = np.zeros(n)
+distances = np.zeros((n,K))
+
+error = np.linalg.norm(centers_new - centers_old)
+
+# When, after an update, the estimate of that center stays the same, exit loop
+while error != 0:
+    # Measure the distance to every center
+    for i in range(K):
+        distances[:,i] = np.linalg.norm(X - centers[i], axis=1)
+    # Assign all training data to closest center
+    clusters = np.argmin(distances, axis = 1)
+    
+    centers_old = deepcopy(centers_new)
+    # Calculate mean for every cluster and update the center
+    for i in range(K):
+        centers_new[i] = np.mean(X[clusters == i], axis=0)
+    error = np.linalg.norm(centers_new - centers_old)
+
+centers_new
+ 
+colors=['orange', 'blue', 'green']
+for i in range(n):
+    plt.scatter(X[i, 0], X[i,1], s=7, color = colors[int(y[i])])
+plt.scatter(centers_new[:,0], centers_new[:,1], marker='*', c='g', s=150) 
