3a1190cbeed837681081579ab893b286b382eca2
davdigioia <dadda.digioia@gmail.com>
2020-07-21 11:56:08 +0100

    Add existing file

---
+++ MultiLinearRegression.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 10:31:22 2020
+
+@author: dav
+"""
+
+import numpy as np
+
+import copy
+
+class MultiLinearRegression():
+    def __init__(self):
+        self.coefficients = None
+        self.intercept = None
+
+    def fit(self, x, y):
+        # prepare x and y values for coefficient estimates
+        x = self._transform_x(x)
+        y = self._transform_y(y)
+
+        betas = self._estimate_coefficients(x, y)
+        
+        # intercept becomes a vector of ones
+        self.intercept = betas[0]
+
+        # coefficients becomes the rest of the betas
+        self.coefficients = betas[1:]
+
+    def predict(self, x):
+        '''
+            y = b_0 + b_1*x + ... + b_i*x_i
+        '''
+        predictions = []
+        for index, row in x.iterrows():
+            values = row.values
+
+            pred = np.multiply(values, self.coefficients)
+            pred = sum(pred)
+            pred += self.intercept
+
+            predictions.append(pred)
+
+        return predictions
+
+    def r2_score(self, y_true, y_pred):
+        '''
+            r2 = 1 - (rss/tss)
+            rss = sum_{i=0}^{n} (y_i - y_hat)^2
+            tss = sum_{i=0}^{n} (y_i - y_bar)^2
+        '''
+        y_values = y_true.values
+        y_average = np.average(y_values)
+
+        residual_sum_of_squares = 0
+        total_sum_of_squares = 0
+
+        for i in range(len(y_values)):
+            residual_sum_of_squares += (y_values[i] - y_pred[i])**2
+            total_sum_of_squares += (y_values[i] - y_average)**2
+
+        return 1 - (residual_sum_of_squares/total_sum_of_squares)
+
+    def _transform_x(self, x):
+        x = copy.deepcopy(x)
+        x.insert(0, 'ones', np.ones( (x.shape[0], 1) ))
+        return x.values
+
+    def _transform_y(self, y):
+        y = copy.deepcopy(y)
+        return y.values
+
+    def _estimate_coefficients(self, x, y):
+        '''
+            β = (X^T X)^-1 X^T y
+            Estimates both the intercept and all coefficients.
+        '''
+        xT = x.transpose()
+
+        inversed = np.linalg.inv( xT.dot(x) )
+        coefficients = inversed.dot( xT ).dot(y)
+
+        return coefficients
+    
